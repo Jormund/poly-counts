17624f23f8cf0ba8590cef9be64bb211e795ce82
Jormund <etiennebotek@gmail.com>
2019-11-15 21:28:58 +0100

    2.1.0.201911115.2102

--- poly-counts2.meta.js
+++ poly-counts2.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @id             iitc-plugin-polyCounts2
-// @name           IITC : Poly Counts 2
+// @name           IITC plugin: Poly Counts 2
 // @category       Info
-// @version        2.0.3.20181031.1930
+// @version        2.1.0.201911115.2102
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://raw.githubusercontent.com/Jormund/poly-counts/master/poly-counts2.meta.js
 // @downloadURL    https://raw.githubusercontent.com/Jormund/poly-counts/master/poly-counts2.user.js
-// @description    [2018-10-31-1930] Display a list of all localized portals by level and faction.
+// @description    [2019-11-15-2102] Display a list of all localized portals by level and faction.
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
