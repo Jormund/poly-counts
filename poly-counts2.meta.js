f043d802cf1d723f628c3cf95b7b92fa7e674f30
Jormund <etiennebotek@gmail.com>
2022-11-14 00:03:51 +0100

    Handle Machina faction

--- poly-counts2.meta.js
+++ poly-counts2.meta.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-polyCounts2
 // @name           IITC plugin: Poly Counts 2
 // @category       Info
-// @version        2.1.1.20221113.2221
+// @version        2.2.0.20221113.2359
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://raw.githubusercontent.com/Jormund/poly-counts/master/poly-counts2.meta.js
 // @downloadURL    https://raw.githubusercontent.com/Jormund/poly-counts/master/poly-counts2.user.js
-// @description    [2022-11-13-2221] Counts portals by level and faction inside polygons or search result.
+// @description    [2022-11-13-2359] Counts portals by level and faction inside polygons or search result.
 // @match          https://intel.ingress.com/*
 // @match          https://intel-x.ingress.com/*
 // @match          https://*.ingress.com/intel*
